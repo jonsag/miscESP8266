4a69282bf4babb79edc96f2f679477e0e45b92f2
jonsag <jonsagebrand@gmail.com>
2022-04-25 18:04:25 +0200

    no success yet

--- ESP8266 ESP-01 with SSD1306 OLED display/src/config.h
+++ ESP8266 ESP-01 with SSD1306 OLED display/src/config.h
@@ -1,18 +1,38 @@
 
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-
 // set I2C pins (SDA = GPIO2, SCL = GPIO0), default clock is 100kHz
 #define SDA 2
 #define SCL 0
 
+/*
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+
 #define OLED_RESET 1 // define SSD1306 OLED reset (TX pin)
+
 Adafruit_SSD1306 display(OLED_RESET);
+*/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 #define NUMFLAKES 10
+
 #define XPOS 0
 #define YPOS 1
+
 #define DELTAY 2
 
 #define LOGO16_GLCD_HEIGHT 16
@@ -36,6 +56,8 @@ static const unsigned char PROGMEM logo16_glcd_bmp[] =
      B01110000, B01110000,
      B00000000, B00110000};
 
+/*
 #if (SSD1306_LCDHEIGHT != 64)
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
+*/
--- ESP8266 ESP-01 with SSD1306 OLED display/src/main.cpp
+++ ESP8266 ESP-01 with SSD1306 OLED display/src/main.cpp
@@ -23,12 +23,16 @@ All text above, and the splash screen must be included in any redistribution
 
 void setup()
 {
-  Serial.begin(9600);
+  //Serial.begin(9600);
 
   Wire.begin(SDA, SCL);
 
-  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3D); // initialize with the I2C addr 0x3D (for the 128x64)
+  //Serial.println("Starting display ...");
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    //Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
   // init done
 
   // Wire.setClock(400000L);   // uncomment this to set I2C clock to 400kHz
@@ -36,79 +40,106 @@ void setup()
   // Show image buffer on the display hardware.
   // Since the buffer is intialized with an Adafruit splashscreen
   // internally, this will display the splashscreen.
+  //Serial.println("Splash screen ...");
   display.display();
   delay(2000);
 
   // Clear the buffer.
+  //Serial.println("Clear display ...");
   display.clearDisplay();
 
   // draw a single pixel
+  //Serial.println("Test 1/15: single pixel ...");
   display.drawPixel(10, 10, WHITE);
   // Show the display buffer on the hardware.
   // NOTE: You _must_ call display after making any drawing commands
   // to make them visible on the display hardware!
   display.display();
   delay(2000);
+
   display.clearDisplay();
 
   // draw many lines
+  //Serial.println("Test 2/15: lines ...");
   testdrawline();
   display.display();
   delay(2000);
+
   display.clearDisplay();
 
   // draw rectangles
+  //Serial.println("Test 3/15: rectangles ...");
   testdrawrect();
   display.display();
   delay(2000);
+
   display.clearDisplay();
 
   // draw multiple rectangles
+  //Serial.println("Test 4/15: rectangles ...");
   testfillrect();
   display.display();
   delay(2000);
+
   display.clearDisplay();
 
   // draw multiple circles
+  //Serial.println("Test 5/15: circles ...");
   testdrawcircle();
   display.display();
   delay(2000);
+
   display.clearDisplay();
 
   // draw a white circle, 10 pixel radius
+  //Serial.println("Test 6/15: fill circle ...");
   display.fillCircle(display.width() / 2, display.height() / 2, 10, WHITE);
   display.display();
   delay(2000);
+
   display.clearDisplay();
 
+  //Serial.println("Test 7/15: round rectangle ...");
   testdrawroundrect();
   delay(2000);
+
   display.clearDisplay();
 
+  //Serial.println("Test 8/15: fill round rectangle ...");
   testfillroundrect();
   delay(2000);
+
   display.clearDisplay();
 
+  //Serial.println("Test 9/15: triangle ...");
   testdrawtriangle();
   delay(2000);
+
   display.clearDisplay();
 
+  //Serial.println("Test 10/15: fill triangle ...");
   testfilltriangle();
   delay(2000);
+
   display.clearDisplay();
 
   // draw the first ~12 characters in the font
+  //Serial.println("Test 10/15: characters ...");
   testdrawchar();
   display.display();
   delay(2000);
+
   display.clearDisplay();
 
   // draw scrolling text
+  //Serial.println("Test 11/15: scroll text ...");
   testscrolltext();
   delay(2000);
+
   display.clearDisplay();
 
   // text display tests
+  //Serial.println("Test 12/15: text ...");
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
@@ -121,21 +152,26 @@ void setup()
   display.println(0xDEADBEEF, HEX);
   display.display();
   delay(2000);
+
   display.clearDisplay();
 
   // miniature bitmap display
+  //Serial.println("Test 13/15: bitmap ...");
   display.drawBitmap(30, 16, logo16_glcd_bmp, 16, 16, 1);
   display.display();
   delay(1);
 
   // invert the display
+  //Serial.println("Test 14/15: invert ...");
   display.invertDisplay(true);
   delay(1000);
   display.invertDisplay(false);
   delay(1000);
+
   display.clearDisplay();
 
   // draw a bitmap icon and 'animate' movement
+  //Serial.println("Test 15/15: animate ...");
   testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
 }
 
